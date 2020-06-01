8718c3f0febf944c7d6ca53ab62391a9c6095188
Nick Zombolas <nickzombolas@gmail.com>
2020-06-01 15:42:31 -0400

    database: initial connection setup

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose')
 const app = express()
 app.use(express.json())
 
+const db = require('./config/keys').mongoURL
+
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log("MongoDB Successfully Connected!"))
+	.catch(err => console.log(err))
+
 const port = 5000
 
 app.listen(port, () => console.log('Server listening on port ' + port))
