67da1ac769ca8f50496be760848ae2b10fb58bea
Nick Zombolas <nickzombolas@gmail.com>
2020-06-29 15:05:32 -0400

    client: about page written

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react'
 class About extends Component {
   render(){
     return(
-      <div>This is the about page. More info to come soon!</div>
+      <div className="m-3 p-3">
+        <h1 className="m-3 text-center">About</h1>
+        <div className="m-3 p-3 indent">
+          <p>
+            SongBook is an app designed to help musicians track the songs they are learning.
+            Search through the database of over 5000 songs, and categorized them based on which songs you want to learn, 
+            are currently learning, or have already learned. As you learn to play more songs, you will quickly develop 
+            a SongBook to share with anybody interested in hearing you play. The app currently focuses on classical piano 
+            music, as I am learning to play piano. The database will continue to develop as more music will be added 
+            for other genres as well as music for many instruments besides piano. For more information, view my live SongBook 
+            below, and refer to my GitHub to see the source code.
+          </p>
+        </div>
+      </div>
     )
   }
 }
