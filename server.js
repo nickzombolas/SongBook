8feca3388c4a1d0fa4ec562854bd52244db46873
Nick Zombolas <nickzombolas@gmail.com>
2020-07-21 15:53:49 -0400

    revert

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ if(process.env.NODE_ENV === 'production') {
 	})
 }
 
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log('Server listening on port ' + port))
